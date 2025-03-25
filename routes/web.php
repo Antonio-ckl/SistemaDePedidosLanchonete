02b00e6476bd5cd9213e0493aa92ca77a904eae6
Antonio-ckl <antonio.cklebis@gmail.com>
2025-03-25 15:21:09 -0300

    Login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Livewire\Auth\Login;
 use App\Livewire\Produto\Create as produtoCreate;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Cliente\Create as ClienteCreate;
@@ -9,4 +9,6 @@ use App\Livewire\Cliente\Create as ClienteCreate;
 Route::get('/create/produto', produtoCreate::class);
 
 Route::get('/create/cliente', ClienteCreate::class);
+
+Route::get('/', Login::class)->name('login');
 
